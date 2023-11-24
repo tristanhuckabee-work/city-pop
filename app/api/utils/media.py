b532b177212df47be486d6bf9cc70a8920927098
tristan <thuckabee.work@gmail.com>
2023-11-24 16:29:00 -0600

    removed cloudinary for deployment

--- app/api/utils/media.py
+++ app/api/utils/media.py
@@ -1,36 +1,6 @@
-import os
-import json
-import cloudinary
-import cloudinary.api
-import cloudinary.uploader
-# from .cloudinary_env import cloud_name, api_key, api_secret
-# from dotenv import load_dotenv
-# load_dotenv()
-config = cloudinary.config(
-  # cloud_name, api_key, api_secret,
-  secure=True
-)
-
 
 def upload_image(file):
   """
   Upload an Image and Get it's URL
   """
-  # cloudinary.uploader.upload(file, **options)
-  # cloudinary.uploader.upload(file,
-  #   use_filename = True,
-  #   unique_filename = False
-  # )
-  test = cloudinary.uploader.upload("https://cloudinary-devs.github.io/cld-docs-assets/assets/images/butterfly.jpeg",
-    public_id="quickstart_butterfly",
-    unique_filename=False,
-    overwrite=True
-  )
-
-  # Build the URL for the image and save it in the variable 'srcURL'
-  # srcURL = cloudinary.CloudinaryImage("quickstart_butterfly").build_url()
-
-  # Log the image URL to the console.
-  # Copy this URL in a browser tab to generate the image on the fly.
-  print("****2. Upload an image****\nDelivery URL: ", test, "\n")
-upload_image()
+  pass
