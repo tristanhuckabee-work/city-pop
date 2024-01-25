1ef20a74349819687083cd1f7a6881ec16fec038
tristan <thuckabee.work@gmail.com>
2024-01-25 17:58:42 -0600

    finished splash-page

--- react-app/src/components/main/index.js
+++ react-app/src/components/main/index.js
@@ -5,9 +5,9 @@ import { get_following, get_recommended } from '../../store/follows';
 import { get_my_likes } from '../../store/likes';
 import { getOtherUser } from '../../store/session';
 
-import SongList         from '../section_songs';
-import SideBar          from '../section_aside';
-import Footer           from '../section_footer';
+import SongList from '../section_songs';
+import SideBar from '../section_aside';
+import Footer from '../section_footer';
 
 import './homepage.css';
 
@@ -40,9 +40,45 @@ function Main() {
   }
   return (
     <>
-      <main>
-        Hello, Stranger
-      </main>
+      <section id='splash-header'>
+        <h2>Hello Stranger,</h2>
+        <p>This is a portfolio project for uploading and listening to music by Tristan Huckabee.</p>
+        <div id='splash-cards'>
+          <article className='splash-card'>
+            <h3>Technologies</h3>
+            <hr></hr>
+            <p>SQLite3 Database</p>
+            <p>Flask Back-End</p>
+            <p>React Front-End</p>
+            <p>Node Runtime Environment</p>
+            <br></br>
+            <p>Render.com for Deployment</p>
+            <p>Cloudinary API for File Uploads</p>
+            <p>Git for Versioning</p>
+          </article>
+          <article className='splash-card'>
+            <h3>Features</h3>
+            <hr></hr>
+            <p>Upload Songs via File or URL</p>
+            <p>Comments on Songs</p>
+            <p>Like/Unlike Songs</p>
+            <p>Follow/Unfollow other Users</p>
+            <br></br>
+            <p>Search for Users and Songs.</p>
+            <p>Algorithms to recommend artists or songs similar to your interest!</p>
+          </article>
+          <article className='splash-card'>
+            <h3>In The Future...</h3>
+            <hr></hr>
+            <p>Playlists : Auto-play Multiple Songs</p>
+            <p>Prioritize songs from followed accounts on user's Feed.</p>
+            <p>Improve recommendation algorithms.</p>
+            <br></br>
+            <p>Improve Responsiveness</p>
+            <p>Audio Visualizer?</p>
+          </article>
+        </div>
+      </section>
     </>
   );
 }
